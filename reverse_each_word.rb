b12e4eb0fb85be58844c2287d4cac0f6e3c6df5b
German Cruz <cruzgerman216@yahoo.com>
2020-03-03 22:02:38 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,10 @@
 def reverse_each_word(string)
-   arr = string.split("")
-   return arr
+   arr = string.split("").reverse
+   str = ""
+   arr.each do |element|
+     str += element
+   end
+   return str
 end
 
 puts reverse_each_word("test")
